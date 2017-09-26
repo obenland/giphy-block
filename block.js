b745a9c5a80920a7e3ea89ce90d3012d2fb99b56
Konstantin Obenland <obenland@gmx.de>
2017-09-26 11:56:03 -0700

    9: Add a search form to search Gifs

--- block.js
+++ block.js
@@ -3,26 +3,6 @@
 	var __ = wp.i18n.__;
 
 	/* For later use.
-	var fetchGifs = _.debounce( function fetchGifs( search ) {
-		if ( attributes.fetching ) {
-			return;
-		}
-
-		props.setAttributes( {
-			fetching: true,
-		} );
-
-		$.getJSON( GIPHY_URL + encodeURI( search ) )
-			.success( function fetchSuccess( data ) {
-				props.setAttributes( {
-					fetching: false,
-					matches: data.data
-				} );
-			} )
-			.fail( function fetchFail() {
-				props.setAttributes( { fetching: false } );
-			} );
-	}, 1000 );
 
 	function mapSearchResults( gif ) {
 		var gifImage = wp.element.createElement( 'img', {
@@ -39,24 +19,6 @@
 			}
 		}, gifImage );
 	}
-
-	var searchField = wp.element.createElement( 'input', {
-		type: 'search',
-		key: 'search-field',
-		placeholder: __( 'Enter Search Term Here', 'giphy-block' ),
-		onChange: function onChangeGiphySearch( event ) {
-			fetchGifs( event.target.value );
-		},
-		onKeyUp: function onKeyUpGiphySearch( event ) {
-			// If the esc key is pressed, clear out the field and matches.
-			if ( event.keyCode === 27 ) {
-				event.target.value = '';
-				props.setAttributes( {
-					matches: [],
-				} );
-			}
-		}
-	} );
 
 	var resultsDiv = wp.element.createElement( 'div', {
 		className: 'giphy__results',
@@ -82,6 +44,27 @@
 		edit: function edit( props ) {
 			var attributes = props.attributes;
 
+			var fetchGifs = _.debounce( function fetchGifs( search ) {
+				if ( attributes.fetching ) {
+					return;
+				}
+
+				props.setAttributes( {
+					fetching: true,
+				} );
+
+				$.getJSON( GIPHY_URL + encodeURI( search ) )
+					.success( function fetchSuccess( data ) {
+						props.setAttributes( {
+							fetching: false,
+							matches: data.data
+						} );
+					} )
+					.fail( function fetchFail() {
+						props.setAttributes( { fetching: false } );
+					} );
+			}, 1000 );
+
 			if ( attributes.url ) {
 				return wp.element.createElement( 'img', { src: attributes.url } );
 			} else {
@@ -90,6 +73,25 @@
 					className: 'giphy__placeholder',
 					icon: 'format-image',
 					label: __( 'Search for the perfect GIF!', 'giphy-block' ),
+					children: [
+						wp.element.createElement( 'input', {
+							type: 'search',
+							key: 'search-field',
+							placeholder: __( 'Enter Search Term Here', 'giphy-block' ),
+							onChange: function onChangeGiphySearch( event ) {
+								fetchGifs( event.target.value );
+							},
+							onKeyUp: function onKeyUpGiphySearch( event ) {
+								// If the esc key is pressed, clear out the field and matches.
+								if ( event.keyCode === 27 ) {
+									event.target.value = '';
+									props.setAttributes( {
+										matches: [],
+									} );
+								}
+							}
+						} )
+					]
 				} );
 			}
 		},
